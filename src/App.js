7373f05b1337a02faf5fb461108751df3b1ea7aa
Kanay <kanosss1@gmail.com>
2023-07-06 21:57:59 +0600

    first view of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://dpg.gg/test/calendar.json");
+        console.log(response.data);
+        let date = Object.entries(response.data);
+        console.log(date);
+        console.log(typeof date);
+        setData(date);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Вспомогательная функция для определения цвета блока на основе количества контрибуций
+  const getColor = (contributions) => {
+    if (contributions === 0) {
+      return "#ebedf0";
+    } else if (contributions <= 9) {
+      return "#c6e48b";
+    } else if (contributions <= 19) {
+      return "#7bc96f";
+    } else if (contributions <= 29) {
+      return "#239a3b";
+    } else {
+      return "#196127";
+    }
+  };
+
+  data.map(([key, value]) => (
+    <div key={key} className="contribution-row">
+      <div key={key} style={{ backgroundColor: getColor(value) }}></div>
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contribution-graph">
+      {data.map(([key, value]) => (
+        <div key={key} className="contribution-row">
+          <div
+            key={key}
+            className="contribution-block"
+            style={{ backgroundColor: getColor(value) }}
+          ></div>
+        </div>
+      ))}
+      ;
     </div>
   );
 }
